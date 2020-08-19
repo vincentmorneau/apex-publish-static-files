112c68f037254c20a9fea48f9497810d38fbe007
Vincent Morneau <vincent.morneau@gmail.com>
2020-08-19 13:08:53 -0400

    xo

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ const fs = require('fs');
 
 module.exports = {
 	// Validates a JSON object
-	publish(opts) {
+	publish(options) {
 		// Set defaults
 		const defaults = {
 			sqlclPath: 'sql',
 			destination: 'application'
 		};
-		opts = Object.assign(defaults, opts);
+		options = Object.assign(defaults, options);
 
 		// Validate arguments
-		if (typeof opts.connectString === 'undefined') {
+		if (typeof options.connectString === 'undefined') {
 			throw new TypeError('connectString is required.');
 		}
 
-		if (typeof opts.directory === 'undefined') {
+		if (typeof options.directory === 'undefined') {
 			throw new TypeError('directory is a required argument.');
 		}
 
-		if (typeof opts.appID === 'undefined') {
+		if (typeof options.appID === 'undefined') {
 			throw new TypeError('appID is a required argument.');
 		}
 
-		if (opts.destination.toLowerCase() === 'plugin' && typeof opts.pluginName === 'undefined') {
+		if (options.destination.toLowerCase() === 'plugin' && typeof options.pluginName === 'undefined') {
 			throw new Error('pluginName is a required argument.');
 		}
 
-		if (!fs.existsSync(opts.directory)) {
-			throw new Error(`Directory ${opts.directory} is not a valid path.`);
+		if (!fs.existsSync(options.directory)) {
+			throw new Error(`Directory ${options.directory} is not a valid path.`);
 		}
 
 		const getAllFiles = (files, dir) => {
@@ -49,40 +49,40 @@ module.exports = {
 		};
 
 		const files = [];
-		getAllFiles(files, opts.directory);
+		getAllFiles(files, options.directory);
 
 		if (files.length === 0) {
 			console.log('Directory is empty.');
 		} else {
 			// Execute the upload process
 			try {
-				switch (opts.destination.toLowerCase()) {
+				switch (options.destination.toLowerCase()) {
 					case 'theme':
-						console.log(`Uploading to ${opts.appID} - Theme Files...`);
+						console.log(`Uploading to ${options.appID} - Theme Files...`);
 						break;
 					case 'workspace':
-						console.log(`Uploading to ${opts.appID} - Workspace Files...`);
+						console.log(`Uploading to ${options.appID} - Workspace Files...`);
 						break;
 					case 'plugin':
-						console.log(`Uploading to ${opts.appID} - ${opts.pluginName} - Plugin Files...`);
+						console.log(`Uploading to ${options.appID} - ${options.pluginName} - Plugin Files...`);
 						break;
 					default:
-						console.log(`Uploading to ${opts.appID} - Application Static Files...`);
+						console.log(`Uploading to ${options.appID} - Application Static Files...`);
 				}
 
-				const spawnOpts = [
-					opts.connectString, // Connect string (user/pass@server:port/sid)
+				const spawnOptions = [
+					options.connectString, // Connect string (user/pass@server:port/sid)
 					'@' + path.resolve(__dirname, 'lib/script'), // Sql to execute
 					path.resolve(__dirname, 'lib/distUpload.js'), // Param &1 (js to execute)
-					path.resolve(opts.directory), // Param &2
-					opts.appID, // Param &3
-					opts.destination, // Param &4
-					opts.pluginName // Param &5
+					path.resolve(options.directory), // Param &2
+					options.appID, // Param &3
+					options.destination, // Param &4
+					options.pluginName // Param &5
 				];
 
 				const childProcess = spawnSync(
-					opts.sqlclPath, // SQLcl path
-					spawnOpts, {
+					options.sqlclPath, // SQLcl path
+					spawnOptions, {
 						encoding: 'utf8'
 					}
 				);
